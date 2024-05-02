e0101773cd392d74bd9b3a81f7b2c9792d721ddf
yuchiling <114201495+yuchiling@users.noreply.github.com>
2024-05-02 16:41:46 +0800

    week11

---
+++ week11-1_TRT_again/main.cpp
@@ -0,0 +1,47 @@
+#include <GL/glut.h>
+void myBody()///我的身體
+{
+    glPushMatrix();///備份矩陣
+        glColor3f(1, 0, 0);///紅色的
+        glutWireCube(0.6);///myBody();
+    glPopMatrix();///還原矩陣
+}
+void myArm()
+{
+    glPushMatrix();
+        glColor3f(0, 1, 0);///綠色的
+        glScalef(1, 0.4, 0.4);///myArm();
+        glutWireCube(0.3);
+    glPopMatrix();
+}
+float angle=0;
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    myBody();///我的身體
+    glPushMatrix();
+        glTranslatef(0.3,0.3,0);
+        glRotatef(angle++, 0, 0, 1);
+        glTranslatef(0.15, 0, 0);///(1)把旋轉中心放到正中心
+        myArm();///我的手臂
+    glPopMatrix();
+    glPushMatrix();
+        glTranslatef(-0.3,0.3,0);
+        glRotatef(angle++, 0, 0, 1);
+        glTranslatef(0.15, 0, 0);///(1)把旋轉中心放到正中心
+        myArm();///我的手臂
+    glPopMatrix();
+
+
+	glutSwapBuffers();
+}
+int main(int argc, char*argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
+	glutCreateWindow("week11");
+	glutDisplayFunc(display);
+	glutIdleFunc(display);
+	glutMainLoop();
+}
