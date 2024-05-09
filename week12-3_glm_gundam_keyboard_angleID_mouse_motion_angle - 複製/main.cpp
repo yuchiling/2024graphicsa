0c741130d759b1ea7be6e7889bb86feb89c9b8e9
yuchiling <114201495+yuchiling@users.noreply.github.com>
2024-05-09 16:35:21 +0800

    week12

---
+++ week12-3_glm_gundam_keyboard_angleID_mouse_motion_angle - 複製/main.cpp
@@ -0,0 +1,188 @@
+#include <opencv/highgui.h> ///使用 OpenCV 2.1 比較簡單, 只要用 High GUI 即可
+#include <opencv/cv.h>
+#include <GL/glut.h>
+int myTexture(char * filename)
+{
+    IplImage * img = cvLoadImage(filename); ///OpenCV讀圖
+    cvCvtColor(img,img, CV_BGR2RGB); ///OpenCV轉色彩 (需要cv.h)
+    glEnable(GL_TEXTURE_2D); ///1. 開啟貼圖功能
+    GLuint id; ///準備一個 unsigned int 整數, 叫 貼圖ID
+    glGenTextures(1, &id); /// 產生Generate 貼圖ID
+    glBindTexture(GL_TEXTURE_2D, id); ///綁定bind 貼圖ID
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); /// 貼圖參數, 超過包裝的範圖T, 就重覆貼圖
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT); /// 貼圖參數, 超過包裝的範圖S, 就重覆貼圖
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST); /// 貼圖參數, 放大時的內插, 用最近點
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); /// 貼圖參數, 縮小時的內插, 用最近點
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img->width, img->height, 0, GL_RGB, GL_UNSIGNED_BYTE, img->imageData);
+    return id;
+}
+#include <GL/glut.h>
+#include "glm.h"
+GLMmodel * pmodel = NULL;
+GLMmodel * handA = NULL;
+GLMmodel * handB = NULL;
+GLMmodel * upperA = NULL;
+GLMmodel * lowerB = NULL;
+GLMmodel * upperB = NULL;
+GLMmodel * lowerA = NULL;
+void drawUpperA(void)
+{
+    if (!upperA) {
+	upperA = glmReadOBJ("data/upperA.obj");
+	if (!upperA) exit(0);
+	glmUnitize(upperA);
+	glmFacetNormals(upperA);
+	glmVertexNormals(upperA, 90.0);
+    }
+
+    glmDraw(upperA, GLM_SMOOTH | GLM_TEXTURE);
+}
+void drawLowerA(void)
+{
+    if (!lowerA) {
+	lowerA = glmReadOBJ("data/lowerA.obj");
+	if (!lowerA) exit(0);
+	glmUnitize(lowerA);
+	glmFacetNormals(lowerA);
+	glmVertexNormals(lowerA, 90.0);
+    }
+
+    glmDraw(lowerA, GLM_SMOOTH | GLM_TEXTURE);
+}
+void drawUpperB(void)
+{
+    if (!upperB) {
+	upperA = glmReadOBJ("data/upperB.obj");
+	if (!upperB) exit(0);
+	glmUnitize(upperB);
+	glmFacetNormals(upperB);
+	glmVertexNormals(upperB, 90.0);
+    }
+
+    glmDraw(upperB, GLM_SMOOTH | GLM_TEXTURE);
+}
+void drawLowerB(void)
+{
+    if (!lowerB) {
+	lowerA = glmReadOBJ("data/lowerB.obj");
+	if (!lowerB) exit(0);
+	glmUnitize(lowerB);
+	glmFacetNormals(lowerB);
+	glmVertexNormals(lowerB, 90.0);
+    }
+
+    glmDraw(lowerB, GLM_SMOOTH | GLM_TEXTURE);
+}
+void drawmodel(void)
+{
+    if (!pmodel) {
+	pmodel = glmReadOBJ("data/Gundam.obj");
+	if (!pmodel) exit(0);
+	glmUnitize(pmodel);
+	glmFacetNormals(pmodel);
+	glmVertexNormals(pmodel, 90.0);
+    }
+
+    glmDraw(pmodel, GLM_SMOOTH | GLM_TEXTURE);
+}
+void drawHandA(void)
+{
+    if (!handA) {
+	handA = glmReadOBJ("data/handA.obj");
+	if (!handA) exit(0);
+	glmUnitize(handA);
+	glmFacetNormals(handA);
+	glmVertexNormals(handA, 90.0);
+    }
+
+    glmDraw(handA, GLM_SMOOTH | GLM_TEXTURE);
+}
+void drawHandB(void)
+{
+    if (!handB) {
+	handB = glmReadOBJ("data/handB.obj");
+	if (!handB) exit(0);
+	glmUnitize(handB);
+	glmFacetNormals(handB);
+	glmVertexNormals(handB, 90.0);
+    }
+
+    glmDraw(handB, GLM_SMOOTH | GLM_TEXTURE);
+}
+void myBody() { ///我的身體
+	glPushMatrix(); ///備份矩陣
+        glColor3f(1, 0, 0);///紅色的
+        glutSolidCube(0.6);///myBody();
+	glPopMatrix(); ///還原矩陣
+}
+float angle = 0, da=1; ///加這行, 讓它轉動
+void motion(int x, int y){
+    angle=y;
+}
+void display()
+{
+    ///angle+=da;
+    ///if(angle>90) da=-1;
+    ///if(angle<0) da=+1;
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glDisable(GL_TEXTURE_2D);
+    ///myBody();///先註解掉身體
+    glutSolidSphere(0.1, 30, 30);///圓球
+    glEnable(GL_TEXTURE_2D);
+    glColor3f(1,1,1);
+
+    glPushMatrix();
+        ///glTranslatef();///掛在肩上
+        glRotatef(angle, 0, 0, 1);
+        glTranslatef(0, -0.07, 0);
+        drawUpperA();///(3)上手臂放好
+        glPushMatrix(); ///加這行, 讓它轉動
+            glTranslatef(-0.02, -0.09, 0);///(3)再掛到上手臂的位置ˋ
+            ///glRotatef(angle, 1, 0, 0);///(2) z軸轉
+            glTranslatef(0, -0.21, 0);///(1)
+            drawLowerA();///(0)
+        glPopMatrix(); ///(0)
+    glPopMatrix();
+    glutSwapBuffers();
+}
+
+const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
+const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat light_position[] = { 2.0f, 5.0f, -5.0f, 0.0f };///加這行, 讓它轉動
+
+const GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
+const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
+const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat high_shininess[] = { 100.0f };
+
+int main(int argc, char*argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_DEPTH);
+    glutCreateWindow("week12 Gundam");
+    glutDisplayFunc(display);
+    glutIdleFunc(display); ///加這行, 讓它轉動
+    glutMotionFunc(motion);
+    myTexture("data/Diffuse.jpg");
+
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+
+    glEnable(GL_LIGHT0);
+    glEnable(GL_NORMALIZE);
+    glEnable(GL_COLOR_MATERIAL);
+    glEnable(GL_LIGHTING);
+
+    glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+    glMaterialfv(GL_FRONT, GL_AMBIENT,   mat_ambient);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE,   mat_diffuse);
+    glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
+    glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
+
+    glutMainLoop();
+}
